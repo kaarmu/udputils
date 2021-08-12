5975e008a49e03700393aa0e535c12b2fef94c13
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2021-08-13 00:09:40 +0200

    fun with the communicator

--- communicators.py
+++ communicators.py
@@ -1,7 +1,7 @@
 
 from socket import socket, AF_INET, SOCK_DGRAM
 from collections.abc import Callable
-from typing import Optional
+from typing import Optional, Union
 import itertools
 
 SockAddr = tuple[str, int]
@@ -16,9 +16,16 @@ class Communicator:
     process_incoming: list[MsgProc]
     process_outgoing: list[MsgProc]
 
-    def __init__(self, local: SockAddr = None, timeout: Optional[int] = None):
+    @classmethod
+    def Any(cls, *args, **kwargs):
+        return cls(('', 0), *args, **kwargs)
+
+    def __init__(self, local: SockAddr, timeout: Optional[int] = None):
         self.remotes = []
         self.local = local or ('', 0)
+
+        self.process_incoming = []
+        self.process_outgoing = []
 
         self.socket = socket(AF_INET, SOCK_DGRAM)
         self.socket.settimeout(timeout)
@@ -27,18 +34,19 @@ class Communicator:
     def __del__(self):
         self.socket.close()
 
-    def send(self, msg: bytes, *targets: int, flags: int = 0) -> int:
+    def send(self, msg: bytes, *targets: Union[int, slice], flags: int = 0) -> int:
         targets = targets or range(len(self.remotes))
 
         for proc in self.process_outgoing:
             msg = proc(msg)
 
+        # need to flatten remotes
         for remote in map(lambda i: self.remotes[i], targets):
             self.socket.sendto(msg, flags, remote)
 
         return len(msg)
 
-    def recv(self, bufsize: int, flags: int = 0) -> bytes:
+    def recv(self, bufsize: int, *, flags: int = 0) -> bytes:
         msg, addr = self.socket.recvfrom(bufsize, flags)
 
         for proc in self.process_incoming:
